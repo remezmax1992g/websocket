7c052de7a7ee510b8690edc50ce7061abfe8855d
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-20 22:24:47 +0300

    create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
+type UserType = {
+    id: number
+    name: string
+    message: string
+    photo: string
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState<string>("")
+    const [users, setUsers] = useState<UserType[]>([{
+        id: 1,
+        name: "Max",
+        message: "Hy guys",
+        photo: "https://via.placeholder.com/40"
+    }])
+    const sendMessage = () => {
+        alert(message)
+    }
+    const changeMessage = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.currentTarget.value)
+    }
+    return (
+        <div className="chat">
+            <div className="messages">
+                {users.map(user =>
+                    <div key={user.id} className="message">
+                        <img src={user.photo}/> <b>{user.name}</b><span>{user.message}</span>
+                    </div>
+                )}
+            </div>
+            <div className="footer">
+                <textarea value={message} onChange={changeMessage}/>
+                <button onClick={sendMessage}>SEND MESSAGE</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
