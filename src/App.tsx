8d4240068cb4f86b66ea3d36d17220229aae9b72
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-21 02:58:37 +0300

    rendering of users

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 
 type UserType = {
-    id: number
+    userId: number
     userName: string
     message: string
     photo: string
@@ -10,19 +10,17 @@ type UserType = {
 
 function App() {
     let [message, setMessage] = useState<string>("")
-   let [users, setUsers] = useState<UserType[]>([{
-        id: 1,
-        userName: "Max",
-        message: "Hy guys",
-        photo: "https://via.placeholder.com/40"
-    }])
+   let [users, setUsers] = useState<UserType[]>([])
     let[ws, setWS] = useState<WebSocket | null>(null)
+    if(ws){
+        ws.onmessage = (messageEvent) => {
+            let usersData = JSON.parse(messageEvent.data)
+            setUsers([...users, ...usersData])
+        }
+    }
+
     useEffect(() => {
        let localWS = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
-        localWS.onmessage = (messageEvent) => {
-           let users = JSON.parse(messageEvent.data)
-            setUsers(users)
-        }
         setWS(localWS)
     },[])
     const sendMessage = () => {
@@ -36,8 +34,8 @@ function App() {
     return (
         <div className="chat">
             <div className="messages">
-                {users.map(user =>
-                    <div key={user.id} className="message">
+                {users.map((user, index) =>
+                    <div key={index} className="message">
                         <img src={user.photo}/> <b>{user.userName}: </b><span>{user.message}</span>
                     </div>
                 )}
