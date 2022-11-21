ee6139a4d34eb3777fc1fa389515f09d26443bc0
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-21 03:31:06 +0300

    scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
 import './App.css';
 
 type UserType = {
@@ -10,39 +10,48 @@ type UserType = {
 
 function App() {
     let [message, setMessage] = useState<string>("")
-   let [users, setUsers] = useState<UserType[]>([])
-    let[ws, setWS] = useState<WebSocket | null>(null)
-    if(ws){
+    let [users, setUsers] = useState<UserType[]>([])
+    let [ws, setWS] = useState<WebSocket | null>(null)
+    let messagesBlockRef = useRef<HTMLDivElement | null>(null)
+    if (ws) {
         ws.onmessage = (messageEvent) => {
             let usersData = JSON.parse(messageEvent.data)
             setUsers([...users, ...usersData])
+            if(messagesBlockRef.current){
+                messagesBlockRef?.current.scrollTo(0, messagesBlockRef.current.scrollHeight)
+            }
+
         }
+
     }
 
     useEffect(() => {
-       let localWS = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+        let localWS = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
         setWS(localWS)
-    },[])
+    }, [])
     const sendMessage = () => {
-        if(ws){
+        if (ws) {
             ws.send(message)
+            setMessage("")
         }
     }
     const changeMessage = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setMessage(e.currentTarget.value)
     }
     return (
-        <div className="chat">
-            <div className="messages">
-                {users.map((user, index) =>
-                    <div key={index} className="message">
-                        <img src={user.photo}/> <b>{user.userName}: </b><span>{user.message}</span>
-                    </div>
-                )}
-            </div>
-            <div className="footer">
-                <textarea value={message} onChange={changeMessage}/>
-                <button onClick={sendMessage}>SEND MESSAGE</button>
+        <div className="app">
+            <div className="chat">
+                <div className="messages" ref={messagesBlockRef}>
+                    {users.map((user, index) =>
+                        <div key={index} className="message">
+                            <img src={user.photo}/> <b>{user.userName}: </b><span>{user.message}</span>
+                        </div>
+                    )}
+                </div>
+                <div className="footer">
+                    <textarea value={message} onChange={changeMessage}/>
+                    <button onClick={sendMessage}>SEND MESSAGE</button>
+                </div>
             </div>
         </div>
     );
